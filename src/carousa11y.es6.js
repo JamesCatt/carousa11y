e4b9a248007b946697dbc2151d823f24f5ca6541
James Catt <jcatt@evergreen.ca>
2018-10-15 12:27:13 -0400

    Improve style of button icons for visibility and state

--- src/carousa11y.es6.js
+++ src/carousa11y.es6.js
@@ -440,7 +440,7 @@ export default class Carousa11y {
 
                     "<span class='u-display--screenreader-only'>Go to previous slide</span>" +
 
-                    "<!-- Zondicons by Steve Schroger - http://www.zondicons.com/ -->" +
+                    "<!-- Zondicons by Steve Schroger (modified) - http://www.zondicons.com/ -->" +
                     "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' class='c-carousa11y__button-icon' aria-hidden='true' role='presentation'><path d='M7.05 9.293L6.343 10 12 15.657l1.414-1.414L9.172 10l4.242-4.243L12 4.343z'/></svg>" +
 
                 "</button>" +
@@ -453,7 +453,7 @@ export default class Carousa11y {
 
                     "<span class='u-display--screenreader-only'>Go to next slide</span>" +
 
-                    "<!-- Zondicons by Steve Schroger - http://www.zondicons.com/ -->" +
+                    "<!-- Zondicons by Steve Schroger (modified) - http://www.zondicons.com/ -->" +
                     "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' class='c-carousa11y__button-icon' aria-hidden='true' role='presentation'><path d='M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z'/></svg>" +
 
                 "</button>" +
